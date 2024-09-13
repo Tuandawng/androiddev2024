733890fc149ce508d5f9ceb1afb2567d8f615e9d
Ngô Quý Đăng Tuấn <dangtuannq@gmail.com>
2024-09-13 16:14:34 +0700

    Practical 8

--- USTH Weather/app/src/main/java/vn/edu/usth/usthweather/WeatherActivity.java
+++ USTH Weather/app/src/main/java/vn/edu/usth/usthweather/WeatherActivity.java
@@ -1,5 +1,6 @@
 package vn.edu.usth.usthweather;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
 import android.util.Log;
 
@@ -9,26 +10,41 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
-import androidx.fragment.app.FragmentManager;
 import androidx.viewpager2.widget.ViewPager2;
 
-import java.util.zip.GZIPInputStream;
+import com.google.android.material.tabs.TabLayout;
+import com.google.android.material.tabs.TabLayoutMediator;
 
 import vn.edu.usth.usthweather.adapter.WeatherAdapter;
 
 public class WeatherActivity extends AppCompatActivity {
     public static final String TAG = "Weather";
-    private ViewPager2 viewPager2;
-    private WeatherAdapter weatherAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        viewPager2 = findViewById(R.id.weather_viewpager);
-        weatherAdapter = new WeatherAdapter(this);
+        ViewPager2 viewPager2 = findViewById(R.id.weather_viewpager);
+        WeatherAdapter weatherAdapter = new WeatherAdapter(this);
         viewPager2.setAdapter(weatherAdapter);
+        TabLayout tabLayout = (TabLayout) findViewById(R.id.tab_layout1);
+        new TabLayoutMediator(tabLayout, viewPager2, new TabLayoutMediator.TabConfigurationStrategy() {
+            @Override
+            public void onConfigureTab(@NonNull TabLayout.Tab tab, int position) {
+                switch(position){
+                    case 0:
+                        tab.setText("HANOI,VIETNAM");
+                        break;
+                    case 1:
+                        tab.setText("PARIS,FRANCE");
+                        break;
+                    case 2:
+                        tab.setText("TOULOUSE,FRANCE");
+                        break;
+                }
+            }
+        }).attach();
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
